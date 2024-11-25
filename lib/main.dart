5e9380ac476ddcb3c5d5f1d24da81f6283fef25b
Faniry1015 <frandriaharimino@yahoo.com>
2024-11-25 13:28:33 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage()));
+  runApp(const MaterialApp(home: HomePage()));
 }
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key});
-
-  final List contacts = [
-    'rakoto bernard',
-    'rabe Jean',
-    'rakoto firinga',
-    'rakoto bernard',
-    'rabe Jean',
-    'rakoto firinga',
-  ];
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: const Text("Container"), elevation: 12, centerTitle : true),
-      body: ListView.separated(
-          itemCount: contacts.length,
-          itemBuilder: (context, index) {
-            return Container(
-                margin: const EdgeInsets.all(10),
-                child: Text(contacts[index]));
-          },
-          separatorBuilder: (context, index) {
-            return const Divider();
-          },),
-    );
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          elevation: 12,
+        ),
+        body: Container(
+            color: Colors.green,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    color: Colors.blue,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text('Body')),
+                const Text('body 2'),
+                Image.network(
+                    'https://solguruz.com/_next/image/?url=https%3A%2F%2Fblog.solguruz.com%2Fwp-content%2Fuploads%2F2023%2F11%2FFlutter-for-Hybrid-Apps-Why-Flutter-is-the-Best-Platform-to-Make-Hybrid-Apps.png&w=1080&q=75',
+                    width: 250)
+              ],
+            )));
   }
 }
