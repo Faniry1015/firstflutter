d5fbe377a38214592391acd71c65535622be2b5a
Faniry1015 <frandriaharimino@yahoo.com>
2024-11-25 08:52:19 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(MaterialApp(home: HomePage()));
 }
 
 class HomePage extends StatelessWidget {
+  HomePage({super.key});
 
-  const HomePage({super.key});
+  final List contacts = [
+    'rakoto bernard',
+    'rabe Jean',
+    'rakoto firinga',
+    'rakoto bernard',
+    'rabe Jean',
+    'rakoto firinga',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Bonjour", textDirection: TextDirection.ltr),
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Container"), elevation: 12, centerTitle : true),
+      body: ListView.separated(
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            return Container(
+                margin: const EdgeInsets.all(10),
+                child: Text(contacts[index]));
+          },
+          separatorBuilder: (context, index) {
+            return const Divider();
+          },),
     );
   }
 }
