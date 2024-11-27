a3a5355c960bd00847fcc6d884870c8b392bd658
Faniry1015 <frandriaharimino@yahoo.com>
2024-11-27 06:05:24 +0300

    update

--- lib/pages/mpivarotra.dart
+++ lib/pages/mpivarotra.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class Mpivarotra extends StatefulWidget {
@@ -8,21 +9,58 @@ class Mpivarotra extends StatefulWidget {
 }
 
 class _MpivarotraState extends State<Mpivarotra> {
+
+  String mpivarotra = '';
+  List countries = [];
+
+final dio = Dio(); 
+
+   getAllCountries() async {
+    try {
+    final response = await dio.get('https://restcountries.com/v3.1/independent?status=true');
+    setState(() => countries = response.data,);
+    print(response);
+    } catch(e){
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        const Text('Mombamomba ny mpivarotra',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-        const TextField(
-          decoration: InputDecoration(
+        Text('Ny anaran\'ny mpivarotra nosoratanao dia i : $mpivarotra',
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+        TextField(
+          decoration: const InputDecoration(
               prefixIcon: Icon(Icons.mail),
               hintText: 'mpivarotra',
               labelText: 'Soraty ny anaran\'ny mpivarotra',
-              border: OutlineInputBorder()),
-              
+              helperText: 'iza ilay mpivarotra ?',
+              border: OutlineInputBorder(),
+              filled: true,
+              fillColor: Colors.grey),
+          onChanged: (value) {
+            setState(() {
+              mpivarotra = value;
+            });
+          },
         ),
-        IconButton(onPressed: () {}, icon: const Icon(Icons.send))
+        IconButton(onPressed: () {}, icon: const Icon(Icons.send)),
+        ElevatedButton(onPressed: getAllCountries, child: const Text('Afficher le nom de tous les pays du monde')),
+        SizedBox(
+        height: 1000,
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: countries.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Image.network(countries[index]['flags']['png'], width: 50, height: 50,),
+              title: Text(countries[index]['name']['common']),
+              subtitle: Text(countries[index]['name']['official']),
+            );
+          },
+        ))
       ],
     );
   }
